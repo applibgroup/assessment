814db9947c54f18ed6ac1718363507abbcd5f6e5
Subham Karmakar <riju29071999@iitkgp.ac.in>
2021-05-22 02:45:27 +0530

    Advanced Assignment News API - Volley not allowed

--- Android/Subham_Karmakar/Advanced/News/app/src/main/java/com/example/news/MainActivity.java
+++ Android/Subham_Karmakar/Advanced/News/app/src/main/java/com/example/news/MainActivity.java
@@ -13,6 +13,9 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.request.EverythingRequest;
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,7 +23,7 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity
 {
     RequestQueue queue;
-    String url = "https://jsonplaceholder.typicode.com/posts";
+    String url = "https://raw.githubusercontent.com/curiousily/simple-quiz/master/script/statements-data.json";
 
 
     @Override
@@ -29,20 +32,43 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        queue = MySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, response ->
-        {
-            Log.d("ABC", "Hi");
-                            Log.d("ABCD", response.toString());
-        }, new Response.ErrorListener()
-        {
-            @Override
-            public void onErrorResponse(VolleyError error)
-            {
-                Log.d("ABC", error.toString());
-            }
-        });
-        MySingleton.getInstance(this).addToRequestQueue(jsonArrayRequest);
+
+
+        NewsApiClient newsApiClient = new NewsApiClient("8987e832944046e2b543ec9baee0feb9");
+
+// /v2/everything
+        newsApiClient.getEverything(
+                new EverythingRequest.Builder()
+                        .q("trump")
+                        .build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+                        Log.d("ABC",response.getArticles().get(0).getTitle());
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.d("ABC", throwable.toString());
+                    }
+                }
+        );
+
+//        queue = MySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
+//        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, response ->
+//        {
+//            Log.d("ABC", "Hi");
+//            System.out.println("Hi Bye");
+////                            Log.d("ABC", response.getJSONArray("articles"));
+//        }, new Response.ErrorListener()
+//        {
+//            @Override
+//            public void onErrorResponse(VolleyError error)
+//            {
+//                Log.d("ABC", error.toString());
+//            }
+//        });
+//        MySingleton.getInstance(this).addToRequestQueue(jsonArrayRequest);
 
 //        queue.add(jsonObjectRequest);
     }
