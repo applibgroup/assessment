80e14c25ca2075c93d9fa73fbeb792765d7562c1
Subham Karmakar <riju29071999@iitkgp.ac.in>
2021-05-22 14:59:52 +0530

    Advanced Assignment

--- Android/Subham_Karmakar/Advanced/News/app/src/main/java/com/example/news/MainActivity.java
+++ Android/Subham_Karmakar/Advanced/News/app/src/main/java/com/example/news/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.news;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +16,8 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.news.adapter.RecyclerViewAdapter;
+import com.example.news.model.Post;
 import com.kwabenaberko.newsapilib.NewsApiClient;
 import com.kwabenaberko.newsapilib.models.request.EverythingRequest;
 import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
@@ -21,20 +25,35 @@ import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity
 {
     private TextView news;
     private String headlines = "";
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private ArrayList<Post> postList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        postList.add(new Post("aa","aa","aa", "aa", "a"));
+        postList.add(new Post("aa","aa","aa", "aa", "abb"));
+
+
+        recyclerViewAdapter = new RecyclerViewAdapter(postList, MainActivity.this);
+        recyclerView.setAdapter(recyclerViewAdapter);
 
 //        news = findViewById(R.id.news);
         NewsApiClient newsApiClient = new NewsApiClient("8987e832944046e2b543ec9baee0feb9");
-
 
         newsApiClient.getEverything(
                 new EverythingRequest.Builder()
@@ -45,6 +64,7 @@ public class MainActivity extends AppCompatActivity
                     public void onSuccess(ArticleResponse response) {
                         for (int i = 0; i <response.getArticles().size() ; i++)
                         {
+
                             headlines = response.getArticles().get(i).getTitle() + "\n";
 //                            headlines = response.getArticles().get(i).getDescription() + "\n\n";
 //                            news.append(headlines);
--- Android/Subham_Karmakar/Advanced/News/app/src/main/java/com/example/news/adapter/RecyclerViewAdapter.java
+++ Android/Subham_Karmakar/Advanced/News/app/src/main/java/com/example/news/adapter/RecyclerViewAdapter.java
@@ -1,37 +1,65 @@
 package com.example.news.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.news.R;
+import com.example.news.model.Post;
+
+import java.util.ArrayList;
+
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>
 {
+    private ArrayList<Post> postsList;
+    private Context context;
+
+    public RecyclerViewAdapter(ArrayList<Post> postsList, Context context)
+    {
+        this.postsList = postsList;
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.news_card,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position)
     {
+        Post post = postsList.get(position);
 
+        holder.title.setText(post.getTitle());
+        holder.description.setText(post.getDescription());
     }
 
     @Override
     public int getItemCount()
     {
-        return 0;
+        return postsList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder
     {
+        public TextView title;
+        public TextView description;
+
         public ViewHolder(@NonNull View itemView)
         {
             super(itemView);
+
+            title = itemView.findViewById(R.id.post_title);
+            description = itemView.findViewById(R.id.post_description);
         }
     }
 }
