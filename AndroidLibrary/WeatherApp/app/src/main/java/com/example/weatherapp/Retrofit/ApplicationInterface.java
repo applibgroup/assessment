5ab0a23eaf85fa87779ebb4eb979cf9901503e6c
jaswanth <jaswanthrampula625@gmail.com>
2021-05-31 18:10:33 +0530

    Weather App

---
+++ AndroidLibrary/WeatherApp/app/src/main/java/com/example/weatherapp/Retrofit/ApplicationInterface.java
@@ -0,0 +1,10 @@
+package com.example.weatherapp.Retrofit;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApplicationInterface {
+    @GET("weather?appid=92756c24107bc39dd0a7541f66ba55c5&units=metric")
+    Call<Example> getWeatherData(@Query("q") String name);
+}
---
+++ AndroidLibrary/WeatherApp/app/src/main/java/com/example/weatherapp/Retrofit/Main.java
@@ -0,0 +1,39 @@
+package com.example.weatherapp.Retrofit;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Main {
+    @SerializedName("temp")
+    String temp;
+
+    @SerializedName("humidity")
+    String humidity;
+
+    @SerializedName("feels_like")
+    String feels_like;
+
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setTemp(String temp) {
+        this.temp = temp;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getFeels_like() {
+        return feels_like;
+    }
+
+    public void setFeels_like(String feels_like) {
+        this.feels_like = feels_like;
+    }
+}
