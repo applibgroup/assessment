e75f8bb8acc1cf7dd14ec4d142643cc898d12283
Jawahar Venugopal <jawahar456@gmail.com>
2021-06-03 01:17:32 +0530

    HMOS Day 1 assignment added Custom Buttons-Jawahar V

--- HarmonyOS/Day1 Assingment/Jawahar Venugopal/entry/src/main/java/com/applib/jawahar_venugopal/hmosassignment/slice/MainAbilitySlice.java
+++ HarmonyOS/Day1 Assingment/Jawahar Venugopal/entry/src/main/java/com/applib/jawahar_venugopal/hmosassignment/slice/MainAbilitySlice.java
@@ -1,6 +1,7 @@
 package com.applib.jawahar_venugopal.hmosassignment.slice;
 
 import com.applib.jawahar_venugopal.hmosassignment.ResourceTable;
+import com.applib.jawahar_venugopal.hmosassignment.customview.CustomButton;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
 import ohos.agp.components.Button;
@@ -15,8 +16,8 @@ public class MainAbilitySlice extends AbilitySlice {
     }
 
     private void initViews() {
-        Button signupButton = (Button) findComponentById(ResourceTable.Id_signup_button);
-        Button loginButton = (Button) findComponentById(ResourceTable.Id_login_button);
+        CustomButton signupButton = (CustomButton) findComponentById(ResourceTable.Id_signup_button);
+        CustomButton loginButton = (CustomButton) findComponentById(ResourceTable.Id_login_button);
         signupButton.setClickedListener(list -> present(new SignupAbilitySlice(), new Intent()));
         loginButton.setClickedListener(list -> present(new LoginAbilitySlice(), new Intent()));
     }
