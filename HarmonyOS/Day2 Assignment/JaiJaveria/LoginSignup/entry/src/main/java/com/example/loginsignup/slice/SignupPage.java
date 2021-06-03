ac6bbb679955850c1d180c79353b23c904256757
jai <jaijaveria16@gmail.com>
2021-06-03 23:38:13 +0530

    Email, password, mobile numbers verified. Gender also added in UI. Day2 Assignment done

--- HarmonyOS/Day2 Assignment/JaiJaveria/LoginSignup/entry/src/main/java/com/example/loginsignup/slice/SignupPage.java
+++ HarmonyOS/Day2 Assignment/JaiJaveria/LoginSignup/entry/src/main/java/com/example/loginsignup/slice/SignupPage.java
@@ -9,49 +9,65 @@ import ohos.agp.components.Text;
 import ohos.agp.components.TextField;
 
 public class SignupPage extends AbilitySlice {
+
+    private interface Lambda
+    {
+        boolean checkTextField(String s);
+    }
     @Override
     protected void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_signup_page);
-        Button button= (Button) findComponentById(ResourceTable.Id_xSignupSubmit);
-        button.setClickedListener(new Component.ClickedListener() {
-            @Override
-            public void onClick(Component component) {
-                TextField firstName= (TextField) findComponentById(ResourceTable.Id_xFirstName);
-                if(!correctName(firstName.getText()))
-                {
-                    Text text=(Text) findComponentById(ResourceTable.Id_xFirstNameError);
-                    text.setVisibility(Component.VISIBLE);
-                    return;
-                }
-                TextField lastName= (TextField) findComponentById(ResourceTable.Id_xFirstName);
-                if(!correctName(lastName.getText()))
-                {
-                    Text text=(Text) findComponentById(ResourceTable.Id_xLastNameError);
-                    text.setVisibility(Component.VISIBLE);
-                    return;
-                }
-                TextField email= (TextField) findComponentById(ResourceTable.Id_xEmailId);
-                Text text=(Text) findComponentById(ResourceTable.Id_xEmailIdError);
-                if(!correctEmail(email.getText(), text))
-                {
-                    text.setVisibility(Component.VISIBLE);
-                    retua@, 
-                }
-
-            }
-        });
-
+        setObserver(ResourceTable.Id_xFirstName, ResourceTable.Id_xFirstNameError, this::correctName);
+        setObserver(ResourceTable.Id_xLastName, ResourceTable.Id_xLastNameError, this::correctName);
+        setObserver(ResourceTable.Id_xEmailId, ResourceTable.Id_xEmailIdError, this::correctEmail);
+        setObserver(ResourceTable.Id_xPassword, ResourceTable.Id_xPasswordError, this::correctPassword);
+        setObserver(ResourceTable.Id_xMobileNo, ResourceTable.Id_xMobileError, this::correctNumber);
     }
 
-    private boolean correctEmail(String str, Text text)
-    {
-        String s="([a-zA-Z0-9])*";
-        String s2="([a-zA-Z])*";
-       return str.matches(s+"[@]"+s+"[\\.]"+s2);
+    private boolean correctPassword(String s) {
+        //length of password must be atleast 8
+        return s.length()>=8;
     }
 
     private boolean correctName(String text) {
         return text.matches("([a-zA-Z])*");
+    }
+
+    private boolean correctEmail(String str) {
+        String s = "([a-zA-Z0-9])*";
+        String s2 = "([a-zA-Z])*";
+        return str.matches(s + "[@]" + s + "[\\.]" + s2);
+    }
+
+    private boolean correctNumber(String str) {
+        return str.matches("([0-9])*") && (str.length()==10);
+    }
+
+    public void setObserver(int textFieldID, int errTextID, Lambda l)
+    {
+        TextField textField = (TextField) findComponentById(textFieldID);
+        Text text = (Text) findComponentById(errTextID);
+        Text.TextObserver observer = new Text.TextObserver()
+        {
+            @Override
+            public void onTextUpdated(String s, int i, int i1, int i2)
+            {
+
+                if (!l.checkTextField(textField.getText()))
+                {
+                    text.setVisibility(Component.VISIBLE);
+                    return;
+                }
+                else {
+                    text.setVisibility(Component.HIDE);
+
+                }
+
+
+            }
+        };
+        textField.addTextObserver(observer);
+
     }
 }
