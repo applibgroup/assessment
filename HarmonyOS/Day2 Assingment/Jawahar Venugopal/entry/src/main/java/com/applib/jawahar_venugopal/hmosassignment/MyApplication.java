1331da6d1710956a596751f5e015ea841ebccb28
Jawahar Venugopal <jawahar456@gmail.com>
2021-06-03 20:39:00 +0530

    HMOS Day2 Assignment - Jawahar

---
+++ HarmonyOS/Day2 Assingment/Jawahar Venugopal/entry/src/main/java/com/applib/jawahar_venugopal/hmosassignment/MyApplication.java
@@ -0,0 +1,10 @@
+package com.applib.jawahar_venugopal.hmosassignment;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
