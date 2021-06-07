fa5c3fbff771828b5cdf4eaff26510f45579ec81
Yash Raj Gupta <yashrajgupta98@gmail.com>
2021-06-07 20:11:33 +0530

    Add day5 assessment

---
+++ HarmonyOS/Day5 Assingment/FirstApp/entry/src/main/java/com/example/firstapp/MainAbility.java
@@ -0,0 +1,13 @@
+package com.example.firstapp;
+
+import com.example.firstapp.slice.MainAbilitySlice;
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends Ability {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setMainRoute(MainAbilitySlice.class.getName());
+    }
+}
