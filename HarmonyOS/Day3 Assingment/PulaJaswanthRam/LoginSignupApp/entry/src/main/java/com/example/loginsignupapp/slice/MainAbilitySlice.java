18b1fc1b870d67e32349aba1952aacf48547e9db
Jaswanth <cs1180369@iitd.ac.in>
2021-06-08 16:57:19 +0530

    HMOS Day3 Assignment

---
+++ HarmonyOS/Day3 Assingment/PulaJaswanthRam/LoginSignupApp/entry/src/main/java/com/example/loginsignupapp/slice/MainAbilitySlice.java
@@ -0,0 +1,29 @@
+package com.example.loginsignupapp.slice;
+
+import com.example.loginsignupapp.ResourceTable;
+import ohos.aafwk.ability.AbilitySlice;
+import ohos.aafwk.content.Intent;
+import ohos.agp.components.Button;
+
+public class MainAbilitySlice extends AbilitySlice {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setUIContent(ResourceTable.Layout_ability_main);
+        Button signup_btn = (Button)findComponentById(ResourceTable.Id_signup_btn);
+        signup_btn.setClickedListener(list -> present(new SignupAbilitySlice(), new Intent()));
+
+        Button login_btn = (Button)findComponentById(ResourceTable.Id_login_btn);
+        login_btn.setClickedListener(list -> present(new LoginAbilitySlice(), new Intent()));
+    }
+
+    @Override
+    public void onActive() {
+        super.onActive();
+    }
+
+    @Override
+    public void onForeground(Intent intent) {
+        super.onForeground(intent);
+    }
+}
---
+++ HarmonyOS/Day3 Assingment/PulaJaswanthRam/LoginSignupApp/entry/src/test/java/com/example/loginsignupapp/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.example.loginsignupapp;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
