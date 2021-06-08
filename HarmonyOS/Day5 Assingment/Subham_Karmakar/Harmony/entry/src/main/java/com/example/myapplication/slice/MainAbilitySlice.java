48d9a5bf15d50f28cc99c6b5bc1eaef545d1fbb1
Subham Karmakar <riju29071999@iitkgp.ac.in>
2021-06-08 15:19:59 +0530

    HarmonyOS Day2

--- HarmonyOS/Day5 Assingment/Subham_Karmakar/Harmony/entry/src/main/java/com/example/myapplication/slice/MainAbilitySlice.java
+++ HarmonyOS/Day5 Assingment/Subham_Karmakar/Harmony/entry/src/main/java/com/example/myapplication/slice/MainAbilitySlice.java
@@ -3,17 +3,52 @@ package com.example.myapplication.slice;
 import com.example.myapplication.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
+import ohos.agp.animation.Animator;
+import ohos.agp.animation.AnimatorScatter;
+import ohos.agp.animation.AnimatorValue;
 import ohos.agp.components.Button;
 import ohos.agp.components.Component;
+import ohos.agp.components.Image;
 
-public class MainAbilitySlice extends AbilitySlice {
+public class MainAbilitySlice extends AbilitySlice
+{
+    private Image image;
+    private Button signup;
+    private Button login;
+    private AnimatorValue animatorValue;
+
     @Override
-    public void onStart(Intent intent) {
+    public void onStart(Intent intent)
+    {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_main);
 
-        Button signup = (Button) findComponentById(ResourceTable.Id_signup);
-        Button login = (Button) findComponentById(ResourceTable.Id_login);
+        animatorValue = new AnimatorValue();
+
+        AnimatorScatter scatter = AnimatorScatter.getInstance(getContext());
+        Animator animator = scatter.parse(ResourceTable.Animation_animator_value);
+        if(animator instanceof AnimatorValue)
+        {
+            animatorValue = (AnimatorValue) animator;
+            animatorValue.setLoopedCount(4);
+            animatorValue.setCurveType(Animator.CurveType.BOUNCE);
+            animatorValue.setDuration(2500);
+            animatorValue.setDelay(0);
+        }
+
+        animatorValue.setValueUpdateListener(new AnimatorValue.ValueUpdateListener() {
+            @Override
+            public void onUpdate(AnimatorValue animatorValue, float value)
+            {
+                image.setContentPosition(image.getContentPositionX(), (int) (400*value));
+            }
+        });
+
+        animatorValue.start();
+
+        signup = (Button) findComponentById(ResourceTable.Id_signup);
+        login = (Button) findComponentById(ResourceTable.Id_login);
+        image = (Image) findComponentById(ResourceTable.Id_img);
 
         signup.setClickedListener(new Component.ClickedListener() {
             @Override
@@ -36,7 +71,9 @@ public class MainAbilitySlice extends AbilitySlice {
     }
 
     @Override
-    public void onForeground(Intent intent) {
+    public void onForeground(Intent intent)
+    {
         super.onForeground(intent);
+
     }
 }
