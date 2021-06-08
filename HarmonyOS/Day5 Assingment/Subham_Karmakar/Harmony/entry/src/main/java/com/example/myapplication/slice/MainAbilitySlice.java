4e17519b026c94a34c654ad7bab7a857a4273fc8
Subham Karmakar <riju29071999@iitkgp.ac.in>
2021-06-08 15:23:59 +0530

    HarmonyOS Day5

--- HarmonyOS/Day5 Assingment/Subham_Karmakar/Harmony/entry/src/main/java/com/example/myapplication/slice/MainAbilitySlice.java
+++ HarmonyOS/Day5 Assingment/Subham_Karmakar/Harmony/entry/src/main/java/com/example/myapplication/slice/MainAbilitySlice.java
@@ -30,7 +30,7 @@ public class MainAbilitySlice extends AbilitySlice
         if(animator instanceof AnimatorValue)
         {
             animatorValue = (AnimatorValue) animator;
-            animatorValue.setLoopedCount(4);
+            animatorValue.setLoopedCount(3);
             animatorValue.setCurveType(Animator.CurveType.BOUNCE);
             animatorValue.setDuration(2500);
             animatorValue.setDelay(0);
