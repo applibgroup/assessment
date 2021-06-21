0701e3b6c199c2f8694120ec9508347756a7a11f
BharathDakarapu183 <85013415+BharathDakarapu183@users.noreply.github.com>
2021-06-21 10:43:33 +0530

    all Hmos assignments and advanced android assignment

---
+++ Android/Bharath/Advanced/MyNewsApp/app/src/main/java/com/example/mynewsapp/MainActivity.java
@@ -0,0 +1,103 @@
+//package com.example.mynewsapp;
+
+//import androidx.appcompat.app.AppCompatActivity;
+
+//import android.os.Bundle;
+
+//public class MainActivity extends AppCompatActivity {
+
+ //   @Override
+   // protected void onCreate(Bundle savedInstanceState) {
+     //   super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_main);
+    //}
+//}
+package com.example.mynewsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+//import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.example.mynewsapp.parameter.Articles;
+import com.example.mynewsapp.parameter.Headlines;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    Adapter adapter;
+    final  String API_KEY="de9933f6d1a54349816bafe67d051c28";
+    Button button;
+    ImageButton floatingActionButton;
+    List<Articles> articles=new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView=findViewById(R.id.recyclerView);
+        button=findViewById(R.id.refreshButton);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        final String country=getCountry();
+//        floatingActionButton=(ImageButton)findViewById(R.id.floating);
+//        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//               // Intent intent=new Intent(MainActivity.this,Intro.class);
+//               //startActivity(intent);
+//
+//         }
+//        });
+
+        retrieveJson(country,API_KEY);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retrieveJson(country,API_KEY);
+            }
+        });
+    }
+    public  void retrieveJson(String country,String apiKey)
+    {
+        Call<Headlines> call=Client.getInstance().getApi().getHeadlines(country,apiKey);
+        call.enqueue(new Callback<Headlines>() {
+            @Override
+            public void onResponse(Call<Headlines> call, Response<Headlines> response) {
+                if (response.isSuccessful() && response.body().getArticles()!=null){
+                    articles.clear();
+                    articles=response.body().getArticles();
+
+                    adapter =new Adapter(MainActivity.this, articles);
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Headlines> call, Throwable t) {
+
+                Toast.makeText(MainActivity.this,"There is An Error",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public String getCountry()
+    {
+        Locale locale=Locale.getDefault();
+        String country=locale.getCountry();
+        return country.toLowerCase();
+    }
+}
---
+++ HarmonyOS/Day1 Assingment/Bharath/Day1/entry/src/main/java/com/assignment/myapp/MyApplication.java
@@ -0,0 +1,10 @@
+package com.assignment.myapp;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
