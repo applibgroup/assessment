f9853df06a8d63118af1456130d114aea24f4a24
chakrichokkaku <85270808+chakrichokkaku@users.noreply.github.com>
2021-06-21 09:46:59 +0530

    Android Advanced Assignment

---
+++ Android/ChakradharChokkaku/Advanced/MyNewsApp/app/src/main/java/com/example/mynewsapp/Interface.java
@@ -0,0 +1,16 @@
+package com.example.mynewsapp;
+
+import com.example.mynewsapp.parameter.Headlines;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface Interface {
+    @GET("top-headlines")
+    Call<Headlines> getHeadlines(
+        @Query("country") String country,
+        @Query("apikey") String apikey
+    );
+}
+
