3320586b48577a82e7a45d7c18284cd1362f71da
jjatinggoyal <45630976+jjatinggoyal@users.noreply.github.com>
2021-08-14 23:44:22 +0530

    FastScroll

---
+++ FastScrollRecyclerView/entry/src/main/java/com/jaredrummler/fastscrollrecyclerview/MyApplication.java
@@ -0,0 +1,10 @@
+package com.jaredrummler.fastscrollrecyclerview;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ FastScrollRecyclerView/entry/src/test/java/com/jaredrummler/fastscrollrecyclerview/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.jaredrummler.fastscrollrecyclerview;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
